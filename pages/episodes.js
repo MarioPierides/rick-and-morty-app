98ae8fdc0b0c1b787afb6e4d35e424a3c413bb91
Mario Pierides <mario.pierides@gmx.de>
2022-05-05 16:31:34 +0200

    Add new pages

--- pages/episodes.js
+++ pages/episodes.js
@@ -1,16 +1,10 @@
-import styled from 'styled-components';
 import Link from 'next/link';
 import { StyledButton } from '../components/button';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
-
-export default function Home() {
+export default function Characters() {
   return (
     <>
-      <h1>Rick and Morty API Wiki</h1>
+      <h1>Episodes</h1>
       <StyledButton>
         <Link href="/">
           <a>Home</a>
